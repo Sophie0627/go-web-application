5a241350f0695825970d31849f39779f49e2cc50
Sophie0627 <bolesalavb@gmail.com>
2021-03-06 00:13:53 -0800

    add env

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request)  {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hello World!</h1>"))
 }
 
-func main()  {
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
